8d98796d319cda9cab8d3dc7b36ba8a8ed3e728d
Tim-PC\Tim <TTuinstra@fullsail.edu>
2017-07-29 11:11:58 +0200

    Finished button functionality on Main Menu.

--- Tarat/Assets/Scripts/Managers/SceneManager.cs
+++ Tarat/Assets/Scripts/Managers/SceneManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneManager : Singleton<SceneManager> {
 
     SceneManager() { }
 
+    public Button startButton;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void TaskOnClick () {
+        Debug.Log("you have clicked the button");
 	}
 }
---
+++ Tarat/Assets/Scripts/UI/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+
+    public void exitGame ()
+    {
+        Application.Quit();
+        print("Game has Quit");
+    }
+
+	// Use this for StartGame
+    public void startGame (string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
