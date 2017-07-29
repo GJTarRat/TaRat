04bb18517105f02969abb8b7448935501ca724dd
Tim-PC\Tim <TTuinstra@fullsail.edu>
2017-07-29 13:32:12 +0200

    Working version

--- Tarat/Assets/Scripts/LogicGeneration/Generator.cs
+++ Tarat/Assets/Scripts/LogicGeneration/Generator.cs
@@ -14,4 +14,5 @@ public abstract class Generator : MonoBehaviour {
 
         print(RandInt);
 	}
+
 }
--- Tarat/Assets/Scripts/SceneDisplay/ClientGeneration.cs
+++ Tarat/Assets/Scripts/SceneDisplay/ClientGeneration.cs
@@ -22,5 +22,6 @@ public class ClientGeneration : Generator {
 
         GenerationProcedure(jacket);
         GenerationProcedure(hair);
+
     }
 }
