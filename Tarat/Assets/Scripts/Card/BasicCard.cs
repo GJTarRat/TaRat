3f6cf9b95af02738627795a8e9b6be3401679a78
Querin <kamibpp@gmail.com>
2017-07-29 15:06:28 +0200

    First card remastered

--- Tarat/Assets/Scripts/Card/BasicCard.cs
+++ Tarat/Assets/Scripts/Card/BasicCard.cs
@@ -7,32 +7,39 @@ public class BasicCard : Card {
 
     [SerializeField]
     private Text story;
-    [SerializeField]
-    private Text outcome1;
-    [SerializeField]
-    private Text outcome2;
-    [SerializeField]
-    private Text outcome3;
-    [SerializeField]
-    private Text hint1;
-    [SerializeField]
-    private Text hint2;
-    [SerializeField]
-    private Text hint3;
-    [SerializeField]
-    private Text prediction1;
-    [SerializeField]
-    private Text prediction2;
-    [SerializeField]
-    private Text prediction3;
+    //[SerializeField]
+    //private Text outcome1;
+    //[SerializeField]
+    //private Text outcome2;
+    //[SerializeField]
+    //private Text outcome3;
+    //[SerializeField]
+    //private Text hint1;
+    //[SerializeField]
+    //private Text hint2;
+    //[SerializeField]
+    //private Text hint3;
+    //[SerializeField]
+    //private Text prediction1;
+    //[SerializeField]
+    //private Text prediction2;
+    //[SerializeField]
+    //private Text prediction3;
 
-
-
+    
+    public List<Text> outcomes;
+    public List<Text> hints;
+    public List<Text> predictions;
+    int randomint;
+    
+    
+      
     // Use this for initialization
-    void Start ()
+    void OnEnable ()
     {
-		
-	}
+        randomint = Random.Range(1, 3);
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
--- Tarat/Assets/Scripts/Card/Card.cs
+++ Tarat/Assets/Scripts/Card/Card.cs
@@ -1,20 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Card : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
-    {
+    { 
 
-        private int card_choice1 = Random.Range(0, 13);
-        private int card_choice2 = Random.Range(0, 13);
-        private int card_choice3 = Random.Range(0, 13);
-         
-        
-
-}
+    }
 	
 	// Update is called once per frame
 	void Update ()
