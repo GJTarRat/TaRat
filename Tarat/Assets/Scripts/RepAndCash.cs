f9b9eef26f565754a882ed6b75f98ac5087a5503
Tim-PC\Tim <TTuinstra@fullsail.edu>
2017-07-29 18:59:45 +0200

    Rep and Cash script added

---
+++ Tarat/Assets/Scripts/RepAndCash.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepAndCash : MonoBehaviour {
+
+	public static int myCash;
+	public static int myRep;
+
+	public  int ReputationPoints;
+	public  int CashPoints;
+
+	public bool StatusRich;
+	public bool StatusNorm;
+	public bool StatusPoor;
+
+	public bool StatusAnswer;
+
+	public void UpdateScores () {
+		myCash += CashPoints;
+		myRep += ReputationPoints;
+	}
+
+	// Update is called once per frame
+	public void PointsGain () {
+
+
+		if(StatusRich){
+			myCash += 15;
+			if (StatusAnswer == true){
+				myRep += 15;
+			} else {
+				myRep -= 15;
+			}
+		}
+		else if (StatusNorm) {
+			myCash += 10;
+			if (StatusAnswer == true){
+				myRep += 10;
+			} else {
+				myRep -= 10;
+			}
+		}
+		else if (StatusPoor) {
+			myCash += 5;
+			if (StatusAnswer == true){
+				myRep += 5;
+			} else {
+				myRep -= 5;
+			}
+		}
+	}
+}
