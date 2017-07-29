b4f7d8a3b0cdc7cb0dc69028fcdd3feb39ea17f1
Querin <kamibpp@gmail.com>
2017-07-29 13:18:27 +0200

    MOAR CARDS

--- Tarat/Assets/Scripts/Card/Card.cs
+++ Tarat/Assets/Scripts/Card/Card.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Card : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+
+        private int card_choice1 = Random.Range(0, 13);
+        private int card_choice2 = Random.Range(0, 13);
+        private int card_choice3 = Random.Range(0, 13);
+         
+        
+
+}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
