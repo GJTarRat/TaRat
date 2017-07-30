92d5a59cc789ad164c75e0f450a671e2333be5f4
Tim-PC\Tim <TTuinstra@fullsail.edu>
2017-07-30 10:26:45 +0200

    A lot

--- Tarat/Assets/Scripts/Managers/SceneManager.cs
+++ Tarat/Assets/Scripts/Managers/SceneManager.cs
@@ -7,17 +7,57 @@ public class SceneManager : Singleton<SceneManager> {
 
     SceneManager() { }
 	
-	// Update is called once per frame
-    public void LoadMainLevel (string mainScene) {
-        Application.LoadLevel(mainScene);
-	}
+    [SerializeField] GameObject Scene1;
+    [SerializeField] GameObject Scene2;
+    [SerializeField] GameObject Scene3;
+    [SerializeField] GameObject Scene4;
 
-    public void ExitGame (){
-        Application.Quit();
-        print("The game has quit");
+
+	// Update is called once per frame
+/*    public void LoadMainLevel (string mainScene) {
+        Application.LoadLevel(mainScene);
+	}*/
+
+    public void StartGame (string MainScene2){
+        Application.LoadLevel(MainScene2);
     }
 
-    public void MainMenu (string mainMenu) {
+    public void RollCredits (string Credits) {
+        Scene1.SetActive(true);
+    }
+     
+    /*    public void MainMenu (string mainMenu) {
         Application.LoadLevel(mainMenu);
-    } 
+    } */
+
+    public void InGameSwitch (string CurScene) {
+        if (CurScene == "MainMenu")
+        {
+            Application.LoadLevel(CurScene);
+        }
+        else if (CurScene == "Quit")
+        {
+            Application.Quit();
+            print("The Game has quit");
+        }
+        else if (CurScene == "Scene1")
+        {
+            Scene1.SetActive(true);
+            Scene2.SetActive(false);
+            Scene3.SetActive(false);
+            Scene4.SetActive(false);
+        }
+        else if (CurScene == "Scene2")
+        {
+            Scene2.SetActive(true);
+        }
+        else if (CurScene == "Scene3")
+        {
+            Scene3.SetActive(true);
+        }
+        else if (CurScene == "Scene4")
+        {
+            Scene4.SetActive(true);
+        }
+    }
 }
